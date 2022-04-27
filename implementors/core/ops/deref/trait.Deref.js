aeb383ca8eeaa028bc3360816bb0c4826ff6499f
realaravinth <realaravinth@batsense.net>
2022-04-27 06:33:35 +0000

    Deploying to gh-pages from  @ e757e5258263d1a7126267369da09471e39c112b 🚀

--- implementors/core/ops/deref/trait.Deref.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["pages"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pages/struct.SETTINGS.html\" title=\"struct pages::SETTINGS\">SETTINGS</a>","synthetic":false,"types":["pages::SETTINGS"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
