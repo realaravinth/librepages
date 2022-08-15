902082200d55e4c7ac69a9cd9c9bc24bf4a00887
realaravinth <realaravinth@batsense.net>
2022-08-15 19:26:28 +0530

    fix: create user if it doesn't exist

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,11 +2,17 @@
 
 USER_ID=${LOCAL_USER_ID}
 echo $USER_ID
+LIBREPAGES_USER=librepages
 
 echo "Starting with UID : $USER_ID"
-export HOME=/home/user
+export HOME=/home/$LIBREPAGES_USER
 #adduser --disabled-password --shell /bin/bash --home $HOME --uid $USER_ID user
 #--uid
-sudo useradd --uid $USER_ID -b /home -m -s /bin/bash user
-su - user 
-pages
+
+if id "$1" &>/dev/null; then
+	echo "User $LIBREPAGES_USER exists"
+else
+	useradd --uid $USER_ID -b /home -m -s /bin/bash $LIBREPAGES_USER
+fi
+
+su $LIBREPAGES_USER  -c 'pages'
