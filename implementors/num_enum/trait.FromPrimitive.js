32c5c7f2da0c8c493380d4bd28be3dbe8fde5276
realaravinth <realaravinth@batsense.net>
2022-09-07 19:43:46 +0000

    Deploying to gh-pages from  @ d6fc9ad67f5171479eec36d02edf8b18880b8ece 🚀

---
+++ implementors/num_enum/trait.FromPrimitive.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pages"] = [{"text":"impl FromPrimitive for <a class=\"enum\" href=\"pages/git/enum.GitFileMode.html\" title=\"enum pages::git::GitFileMode\">GitFileMode</a>","synthetic":false,"types":["pages::git::GitFileMode"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ pages/serve/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["find_page",""],["services",""]],"mod":[["routes",""]],"struct":[["index",""]]};
