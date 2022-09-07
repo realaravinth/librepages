3d4d263657b0d9ec4ca518a1424f67a86b0e79a2
realaravinth <realaravinth@batsense.net>
2022-09-07 05:12:18 +0000

    Deploying to gh-pages from  @ 53bd8c54bba0b2cbc27ad576924d3580fb8d5e1e 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var sourcesIndex = {};
-sourcesIndex["pages"] = {"name":"","files":["ctx.rs","deploy.rs","errors.rs","main.rs","meta.rs","page.rs","routes.rs","settings.rs"]};
+sourcesIndex["pages"] = {"name":"","files":["ctx.rs","deploy.rs","errors.rs","git.rs","main.rs","meta.rs","page.rs","routes.rs","serve.rs","settings.rs"]};
 createSourceSidebar();
