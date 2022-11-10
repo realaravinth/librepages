72f2d4096be4cd339ea8d2acc061078255d7701d
realaravinth <realaravinth@batsense.net>
2022-11-10 12:23:52 +0000

    Deploying to gh-pages from  @ 58bb60687943336546656d8665a1b6954eb6fa18 🚀

--- pages/subdomains/sidebar-items.js
+++ pages/subdomains/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["LEN",""],["WORDLIST",""]],"fn":[["get_random_subdomain",""]]};
+window.SIDEBAR_ITEMS = {"constant":[["LEN",""],["WORDLIST",""]],"fn":[["get_random_id",""],["get_random_subdomain",""]],"struct":[["ID",""]]};
