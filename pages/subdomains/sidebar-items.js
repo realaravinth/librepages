9c021631e6929e09dd98ca3827ae5992c72abbf6
realaravinth <realaravinth@batsense.net>
2022-11-10 11:55:06 +0000

    Deploying to gh-pages from  @ 344cc859359d233789a49bc17ce0153332719faf 🚀

---
+++ pages/subdomains/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":[["LEN",""],["WORDLIST",""]],"fn":[["get_random_subdomain",""]]};
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"pages":["",[["api",[["v1",[["account",[],["mod.rs"]]],["auth.rs","mod.rs","routes.rs"]]],["mod.rs"]],["ctx",[["api",[["v1",[],["account.rs","auth.rs","mod.rs","pages.rs"]]],["mod.rs"]]],["mod.rs"]],["pages",[["auth",[],["login.rs","mod.rs","register.rs"]],["dash",[],["home.rs","mod.rs"]]],["errors.rs","mod.rs","routes.rs"]],["static_assets",[],["filemap.rs","mod.rs","static_files.rs"]]],["db.rs","deploy.rs","errors.rs","git.rs","main.rs","meta.rs","page.rs","preview.rs","serve.rs","settings.rs","utils.rs"]]\
+"pages":["",[["api",[["v1",[["account",[],["mod.rs"]]],["auth.rs","mod.rs","routes.rs"]]],["mod.rs"]],["ctx",[["api",[["v1",[],["account.rs","auth.rs","mod.rs","pages.rs"]]],["mod.rs"]]],["mod.rs"]],["pages",[["auth",[],["login.rs","mod.rs","register.rs"]],["dash",[],["home.rs","mod.rs"]]],["errors.rs","mod.rs","routes.rs"]],["static_assets",[],["filemap.rs","mod.rs","static_files.rs"]]],["db.rs","deploy.rs","errors.rs","git.rs","main.rs","meta.rs","page.rs","preview.rs","serve.rs","settings.rs","subdomains.rs","utils.rs"]]\
 }');
 createSourceSidebar();
